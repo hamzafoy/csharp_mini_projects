98a93878887528c51a1edab48351cc4cac16a387
Ross Foy <hamzaoflouisville@gmail.com>
2022-01-15 11:38:11 -0500

    Adjust the program to take user input where the user give the # of hours expected to work to find out what time they will get off their shift.

--- EmployerFriendlyProgram/WorkLengthCalculator.cs
+++ EmployerFriendlyProgram/WorkLengthCalculator.cs
@@ -7,12 +7,18 @@ class WorkLengthCalculator
         //Determine what time you'll get off work.
         //`startHour` collects your current time.
         DateTime startHour = DateTime.Now;
-        //The `TimeSpan` typed variable is given the hours and minutes that the employee expects to work.
-        TimeSpan lengthOfWork = new TimeSpan(12, 11, 00);
+        //The user will input the # of hours he/she expects to work.
+        //The input will be converted from string to int.
+        //The hour will be used in the TimeSpan typed variable
+        string userInputtedHour;
+        Console.Write("How many hours will you work today? ");
+        userInputtedHour = Console.ReadLine();
+        int hourAsInteger = Convert.ToInt32(userInputtedHour);
+        TimeSpan lengthOfWork = new TimeSpan(hourAsInteger, 00, 00);
         //This last variable will add the work time's length to the determined start time.
         //This variable stores the expected time the employee will end their day's shift.
         DateTime endHour = startHour.Add(lengthOfWork);
 
-        Console.WriteLine(endHour);
+        Console.WriteLine("Expect to leave at: {0}", endHour.ToString("hh:mm tt"));
     }
 }
